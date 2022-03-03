d509841a74e82d15cd2af5dacc98063d3b615d22
Rasong1 <rasongrongphar@gmail.com>
2022-03-03 22:51:48 +0530

    demo page

--- pyro_demo/server.js
+++ pyro_demo/server.js
@@ -1,6 +1,7 @@
 const express = require('express')
 const app = express()
 const port = 8000
+const path = require('path')
 
 app.get('/', (req, res) => {
   res.sendFile(path.join(__dirname+"index.html"))
