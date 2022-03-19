fc25a121133b2747f936dbaf60c36f13e0bb68c2
Rasong1 <rasongrongphar@gmail.com>
2022-03-19 17:19:10 +0530

    rawhtml homepage needs review and port in reactjs

--- pyrokinesis-2k22-Chiman1/server.js
+++ pyrokinesis-2k22-Chiman1/server.js
@@ -7,6 +7,8 @@ app.get('/', (req, res) => {
   res.sendFile(path.join(__dirname+"/index.html"))
 })
 
+app.use(express.static('images'))
+
 app.listen(port, () => {
   console.log(`Example app listening on port ${port}`)
 })
